23dfb0b5efc00065501eef7d046f39cfc7e28d35
phez99 <aseptianabdurohman@gmail.com>
2024-06-14 15:39:46 +0700

    reject if username is already

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,7 @@
 import supertest from "supertest";
 import { prismaClient } from "../src/application/database.js";
 import { web } from "../src/application/web.js";
+import { logger } from "../src/application/logging.js";
 
 describe("POST /api/users", function () {
   afterEach(async () => {
@@ -22,5 +23,35 @@ describe("POST /api/users", function () {
     expect(result.body.data.username).toBe("codephez");
     expect(result.body.data.name).toBe("aseptian abdurohman");
     expect(result.body.data.password).toBeUndefined();
+  });
+
+  it("should reject if request is invalid", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+    });
+
+    logger.info(result.body);
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+
+  it("should reject if username is already registered", async () => {
+    let result = await supertest(web).post("/api/users").send({
+      username: "codephez",
+      password: "rahasia",
+      name: "aseptian abdurohman",
+    });
+    logger.info(result.body);
+    result = await supertest(web).post("/api/users").send({
+      username: "codephez",
+      password: "rahasia",
+      name: "aseptian abdurohman",
+    });
+
+    logger.info(result.body);
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
   });
 });
